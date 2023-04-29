9df702a8b9edaeab58ef29552b98e02264ca7170
kyehwan <clon0306@gmail.com>
2023-04-29 22:29:35 +0000

    tailwind 수업

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Section from "./components/Section";
 import Tail from "./components/Tail";
 
 function App() {
